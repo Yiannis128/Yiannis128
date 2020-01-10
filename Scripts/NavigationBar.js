b38f6d1e319fd75b43314b9387deb1080ae3c7d4
Yiannis128 <yiannis128@hotmail.com>
2020-01-10 01:00:17 +0000

    Redid the nav bar button code to fix a log of bugs.

--- Scripts/NavigationBar.js
+++ Scripts/NavigationBar.js
@@ -1,17 +1,15 @@
-function navBarDropDownButtonClick()
+/**
+ * @name onNavBarResize On Navigation Bar Resize
+ * @description The purpose of this function is set the display mode of nav bar when the
+ * window is resized.
+ */
+function onNavBarResize()
 {
-  var navBarItemsListElement = document.getElementsByClassName("NavigationBarItemsList")[0];
-
-  if(!navBarItemsListElement) 
+  var header = document.getElementsByTagName("header")[0];
+  var navBarItemsListElement = document.getElementById("NavigationBarItemsList");
+  
+  if(header.offsetWidth > 1350 || document.navBarExpanded)
   {
-    console.error("Error: No nav bar items list found.");
-    return;
-  }
-
-  //Determine if display is none or not. Also, intially on phones it is not defined (see elements in dev menu).
-  if(navBarItemsListElement.style.display === "none" || !navBarItemsListElement.style.display)
-  {
-    //Menu is collapsed so need to expand it.
     navBarItemsListElement.style.display = "block";
   }
   else
@@ -19,3 +17,44 @@ function navBarDropDownButtonClick()
     navBarItemsListElement.style.display = "none";
   }
 }
+
+/**
+ * @name registerNavBarEvents This function will register all the event listeners that are related to the navigation
+ * bar.
+ */
+function initNavBar()
+{
+  /**
+   * Custom boolean is used to indicate the state of the navigation bar on small screens
+   * When the navigation bar drop down button is pressed, it toggles between true and false.
+   * This is used so the state is retained.
+   */
+  document.navBarExpanded = false;
+  window.onresize = onNavBarResize;
+}
+
+function navBarDropDownButtonClick()
+{
+  var navBarItemsListElement = document.getElementById("NavigationBarItemsList");
+
+  if(!navBarItemsListElement) 
+  {
+    console.error("Error: No element with id 'NavigationBarItemsList' has been found.");
+    return;
+  }
+  
+  //Determine if display is none or not. Also, intially on phones it is not defined (see elements in dev menu).
+  if(navBarItemsListElement.style.display === "none" || !navBarItemsListElement.style.display)
+  {
+    //Menu is collapsed so need to expand it.
+    navBarItemsListElement.style.display = "block";
+    document.navBarExpanded = true;
+  }
+  else
+  {
+    navBarItemsListElement.style.display = "none";
+    document.navBarExpanded = false;
+  }
+}
+
+initNavBar();
