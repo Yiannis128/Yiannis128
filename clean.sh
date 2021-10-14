0289a1ec0e3a2910c65aebae44a9f17d9f005b87
Yiannis Charalambous <yiannis128@hotmail.com>
2021-10-14 09:37:21 +0100

    Added Source/articles/*.html for cleaning

--- clean.sh
+++ clean.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 
-if [ -d "Public" ]
+if [ -d "Public" ];
 then
     rm -r "Public/"
+fi
+
+if [ -d "Source/articles" ] && [ "$(ls Source/articles/*.html | wc -l)" -ge "1" ];
+then
+    for file in Source/articles/*.html; do
+        rm "$file"
+    done
 fi
