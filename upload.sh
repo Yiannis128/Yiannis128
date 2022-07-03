4a75440c6d076eae085c0a7a3682cafb4f5503dc
Yiannis Charalambous <yiannis128@hotmail.com>
2022-07-03 23:23:50 +0100

    added upload script ssh keys

--- upload.sh
+++ upload.sh
@@ -5,4 +5,6 @@ DEST="/home/yiannis/website/"
 
 CHOWN="yiannis:yiannis"
 
-rsync -uvrP --delete-after --chown=nginx:nginx $SRC yiannis@yiannis-charalambous.com:$DEST
+rsync -uvrP --delete-after --chown=nginx:nginx -e \
+"ssh -i ~/.ssh/yiannis-charalambous.com" \
+$SRC yiannis@yiannis-charalambous.com:$DEST
